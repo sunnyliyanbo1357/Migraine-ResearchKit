99ae3b84cf54e0040f471a4afca16d4f91557845
Yanbo Li <yl2556@cornell.edu>
2015-11-22 23:57:05 -0500

    Initial Commit

---
+++ Migraine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Migraine
+//
+//  Created by Yanbo Li on 11/22/15.
+//  Copyright © 2015 Yanbo Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
